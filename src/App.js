7929c91d9994f1a7202f17a3258fb1abd7c22716
praveensindagi <43067328+praveensindagi@users.noreply.github.com>
2026-09-08 21:58:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
           element={<h1>404 - Page Not Found</h1>}
         />
       </Routes>
-            <ResponsiveLayout/>
+            </ResponsiveLayout>
     </BrowserRouter>
   );
 }
